005cc95a972d1f3ed746fa0c496b5e06d0299741
karimDeiss <karim.deiss@fullstackuniverse.com>
2021-10-16 13:12:56 +0300

    Second commit, we added a post request with the endpoint person/add

--- src/server.js
+++ src/server.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/person",(req,res) => {
+    const personObject = {
+        name : "Hussien",
+        age : 19,
+        religion : "Athesiast",
+        education : null
+    };
+    res.status(200).json(personObject);
+});
+
+app.post("/person/add",(req,res) => {
+    const body = req.body;
+    res.status(200).json({success : true,message : "Person was added successfully"});
+})
 
 
 app.listen(3000,"localhost",() => {
